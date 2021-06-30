877259ad9b8b094c987bf50edd299f431a8f9552
Steven Tang <mrsteventang@outlook.com>
2021-06-30 15:11:00 -0400

    Adding test case for timer to prove it is correct. Note that the unit is microseconds rather than milliseconds

---
+++ libScalerHook/tests/src/unittests/TestTimer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <gtest/gtest.h>
+#include <util/tool/timer.h>
+#include <thread>
+
+TEST(Tools, getunixtimestampms) {
+    auto start=getunixtimestampms();
+    std::this_thread::sleep_for (std::chrono::seconds(1));
+    auto end=getunixtimestampms();
+    EXPECT_TRUE(abs(end-start)/1000000 - 1 < 0.00000001);
+
+}
+
+
+
+
