1d7363e4859118526126a7bad951dfdaf11442ec
Steven Tang <mrsteventang@outlook.com>
2022-01-08 15:07:03 -0500

    Adding test case

--- libHook/tests/src/unittests/TestProtobuf.cpp
+++ libHook/tests/src/unittests/TestProtobuf.cpp
@@ -9,8 +9,14 @@
 using namespace scaler::test;
 using namespace std;
 
-// This function fills in a Person message based on user input.
-void PromptForAddress(scaler::test::Person *person) {
+
+TEST(Protobuf, saveandload) {
+    // Verify that the version of the library that we linked against is
+    // compatible with the version of the headers we compiled against.
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    AddressBook address_book;
+    auto person = address_book.add_people();
     int id = 1;
     person->set_id(1);
     person->set_name("Steven");
@@ -20,95 +26,21 @@ void PromptForAddress(scaler::test::Person *person) {
     Person::PhoneNumber *phone_number = person->add_phones();
     phone_number->set_number(number);
     phone_number->set_type(Person::MOBILE);
-}
 
-void ListPeople(const AddressBook &address_book) {
-    for (int i = 0; i < address_book.people_size(); i++) {
-        const Person &person = address_book.people(i);
-
-        std::cout << "Person ID: " << person.id() << std::endl;
-        std::cout << "  Name: " << person.name() << std::endl;
-        if (person.has_email()) {
-            std::cout << "  E-mail address: " << person.email() << std::endl;
-        }
-
-        for (int j = 0; j < person.phones_size(); j++) {
-            const Person::PhoneNumber &phone_number = person.phones(j);
-
-            switch (phone_number.type()) {
-                case Person::MOBILE:
-                    std::cout << "  Mobile phone #: ";
-                    break;
-                case Person::HOME:
-                    std::cout << "  Home phone #: ";
-                    break;
-                case Person::WORK:
-                    std::cout << "  Work phone #: ";
-                    break;
-            }
-            std::cout << phone_number.number() << std::endl;
-        }
-    }
-}
-
-int main() {
-    // Verify that the version of the library that we linked against is
-    // compatible with the version of the headers we compiled against.
-    GOOGLE_PROTOBUF_VERIFY_VERSION;
-
-    AddressBook address_book;
-
-    // Add an address.
-    PromptForAddress(address_book.add_people());
-    ListPeople(address_book);
-
-    fstream output("/home/st/Projects/Scaler/cmake-build-debug/addressBook.bin", ios::out | ios::trunc | ios::binary);
-    if (!address_book.SerializeToOstream(&output)) {
-        cerr << "Failed to write address book." << endl;
-        return -1;
-    }
+    fstream output("addressBook.bin", ios::out | ios::trunc | ios::binary);
+    EXPECT_TRUE(address_book.SerializeToOstream(&output));
     output.close();
 
-
     AddressBook address_book1;
-    fstream input("/home/st/Projects/Scaler/cmake-build-debug/addressBook.bin", ios::in | ios::binary);
-    if (!input) {
-        cout << "addressBook.bin not found.  Creating a new file." << endl;
-    } else if (!address_book1.ParseFromIstream(&input)) {
-        cerr << "Failed to parse address book." << endl;
-        return -1;
-    }
-    ListPeople(address_book1);
-    input.close();
+    fstream input("addressBook.bin", ios::in | ios::binary);
+    EXPECT_TRUE(input);
+    EXPECT_TRUE(address_book1.ParseFromIstream(&input));
 
-
-
-//    //Save to disk
-//    FILE* file=fopen("testAddrBook.bin","w");
-//    if (!address_book.SerializeToFileDescriptor(,)) {
-//        std::cerr << "Failed to write address book." << std::endl;
-//        return -1;
-//    }
-//    output.close();
-//
-//    AddressBook address_book1;
-//    std::fstream input("testAddrBook.bin", std::ios::out | std::ios::trunc | std::ios::binary);
-//    assert(input.good());
-////    if (!address_book1.SerializeToOstream(&)) {
-////        std::cerr << "Failed to write address book." << std::endl;
-////        return -1;
-////    }
-//
-//    input.close();
-//    std::cout<<address_book.people_size()<<std::endl;
-//    std::cout<<address_book1.people_size()<<std::endl;
-//    auto &firstPeople = address_book.people(0);
-////
-////    std::string outputStr;
-////    assert(address_book1.SerializeToString(&outputStr));
-////    firstPeople.SerializeToOstream(&std::cout);
-//
-//    // Optional:  Delete all global objects allocated by libprotobuf.
-//    google::protobuf::ShutdownProtobufLibrary();
-
+    EXPECT_EQ(address_book1.people_size(),1);
+    auto readPerson=address_book1.people(0);
+    EXPECT_EQ(readPerson.id(),1);
+    EXPECT_EQ(readPerson.name(),"Steven");
+    EXPECT_EQ(readPerson.email(),"test@test.com");
+    EXPECT_EQ(readPerson.phones(0).number(),"8652846736");
+    EXPECT_EQ(readPerson.phones(0).type(),Person::MOBILE);
 }
