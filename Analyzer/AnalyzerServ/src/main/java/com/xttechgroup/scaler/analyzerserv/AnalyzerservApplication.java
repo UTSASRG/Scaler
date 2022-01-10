2558e28ea75c112e56cb0dbfdfe4508569fc7655
Steven Tang <mrsteventang@outlook.com>
2022-01-09 19:33:23 -0500

    Adding info rest and rpc api

--- Analyzer/AnalyzerServ/src/main/java/com/xttechgroup/scaler/analyzerserv/AnalyzerservApplication.java
+++ Analyzer/AnalyzerServ/src/main/java/com/xttechgroup/scaler/analyzerserv/AnalyzerservApplication.java
@@ -1,6 +1,5 @@
 package com.xttechgroup.scaler.analyzerserv;
 
-import com.xttechgroup.scaler.analyzerserv.annotation.GrpcService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,11 +10,7 @@ import java.util.Map;
 public class AnalyzerservApplication {
 
     public static void main(String[] args) {
-
-        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(AnalyzerservApplication.class, args);
-        Map<String, Object> grpcServiceBeanMap =  configurableApplicationContext.getBeansWithAnnotation(GrpcService.class);
-        GrpcLauncher grpcLauncher = configurableApplicationContext.getBean("grpcLauncher",GrpcLauncher.class);
-        grpcLauncher.grpcStart(grpcServiceBeanMap);
+        SpringApplication.run(AnalyzerservApplication.class, args);
     }
 
 }
