3a8ef7783b9de4d380915f15e4d7b26b79bdf4ab
Steven Tang <mrsteventang@outlook.com>
2022-01-12 17:26:26 -0500

    Adding 404 page

--- Analyzer/webvisualizer/src/router/index.js
+++ Analyzer/webvisualizer/src/router/index.js
@@ -33,7 +33,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/Run/About.vue')
-  }
+  },
+  {
+    path: '*',
+    name: '404 Error',
+    component: () => import('../views/Errors/404.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ Analyzer/webvisualizer/src/views/Analysis/Analysis.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <v-navigation-drawer v-model="drawer" :permanent="permanent" clipped app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {
+    // Welcome,
+  },
+  prop: {},
+  model: {},
+  data: {},
+  beforeMount() {
+    this.$emit("updateDisplayName", "Welcome");
+  },
+};
+</script>
