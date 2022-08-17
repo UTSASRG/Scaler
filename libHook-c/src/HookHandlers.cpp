7a7911dc60a24ae06d55eb4e90e126b2311531c5
Steven Tang <mrsteventang@outlook.com>
2022-08-17 15:50:45 +0000

    Finish scheme 2

--- libHook-c/src/HookHandlers.cpp
+++ libHook-c/src/HookHandlers.cpp
@@ -330,6 +330,8 @@ void __attribute__((used, naked, noinline)) callIdSaver() {
     //Jmp to prehookHandler
     "movq $0x1122334455667788,%r11\n\t"
     "jmpq *%r11\n\t"
+
+    //A 64 bit number
     );
 }
 
@@ -340,10 +342,19 @@ void __attribute__((used, naked, noinline)) callIdSaver() {
  */
 void __attribute__((used, naked, noinline,optimize(0))) callIdSaver1() {
     __asm__ __volatile__ (
+    "lock addq $0x1,0x29(%rip)\n\t" //Atomic function counting
+
     "movq $0x1122334455667788,%r11\n\t"
     "jmpq (%r11)\n\t"
     "pushq $0x11223344\n\t"
     "movq $0x1122334455667788,%r11\n\t"
     "jmpq *%r11\n\t"
+    );
+}
+
+void __attribute__((used, naked, noinline)) atomicAdd() {
+    __asm__ __volatile__ (
+    //Save Id to memory (Redzone 128+register 128+ funcId 8 + alignment 8)
+    "lock addq $0x1,0x11223344(%rip)\n\t"
     );
 }
