87e78310d29156479d3c82b995946a2207dccd9e
Steven Tang <mrsteventang@outlook.com>
2022-08-19 02:28:04 +0000

    Use a microbenchmark to prove mem write and mem read speed is the same for simple program

---
+++ libHook-c/tests/src/demoapps/TestBinaryAccess.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <atomic>
+#include <cmath>
+
+std::atomic<int> foobar(8);
+
+inline uint64_t getunixtimestampms() {
+    uint32_t lo, hi;
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((int64_t) hi << 32) | lo;
+}
+
+int main() {
+    int *array = (int *) malloc(sizeof(int) * 4);
+    uint64_t start = getunixtimestampms();
+    for (int i = 0; i < pow(10,5); ++i) {
+        __asm__ (
+        "movq %%r11,(%0)"
+        ://Output
+        : "r" (array)//Input
+        : "r11"//Clobbers
+        );
+    }
+    uint64_t stop = getunixtimestampms();
+
+    uint64_t duration = stop - start;
+    printf("Mem write %lu\n", duration);
+
+
+    start = getunixtimestampms();
+    for (int i = 0; i < pow(10,5); ++i) {
+        __asm__ (
+        "movq (%0),%%r11"
+        ://Output
+        : "r" (array)//Input
+        : "r11"//Clobbers
+        );
+    }
+    stop = getunixtimestampms();
+
+    duration = stop - start;
+    printf("Mem read %lu\n", duration);
+
+    return 0;
+}
